76e56b3f21c4f54d948bb2c28352bfcfd18316f1
Adilhon06 <adubaichik2006@gmail.com>
2023-12-23 16:26:05 +0600

    changed

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         double[] arrayDouble = {23.3, 18.2, -2.3, 9.4, 8.3, -6.3, 6.7, 9.2, 7.7, -4.2, -5.6, 4.5, -6.9, 8.7, -2.5};
+        int count = 0;
         double sum = 0;
 
-        for (double number : arrayDouble){
-            if (number == 23.3 || number == 18.2 || number <= 0) {
-                continue;
-            }
+        boolean negative = false;
 
+        for (double number : arrayDouble){
+            if (number <= 0) {
+                negative = true;
+            }
+            if (negative && number >= 0){
+                count ++;
+            }
             sum += number;
         }
-        sum = sum / 7;
-        System.out.println(sum);
+        System.out.println(sum / count);
     }
 }
